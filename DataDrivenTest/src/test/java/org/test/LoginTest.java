70ebed79e7889c9b19bb7166f8b078afbed7e0a8
manimaran4509744 <akash4509744@gmail.com>
2023-01-28 11:11:39 +0530

    This is second commit

---
+++ DataDrivenTest/src/test/java/org/test/LoginTest.java
@@ -0,0 +1,17 @@
+package org.test;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	@Test
+	private void validCredentials() {
+		
+
+	}
+	@Test
+	private void invalidCredentials() {
+		
+
+	}
+
+}
